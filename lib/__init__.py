75bdf34954a5cc59d6fa7df5dfc78edd148cf596
TylerCharlton <TyCharlton1993@gmail.com>
2024-02-06 22:49:49 -0500

    finished

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,27 @@
 
 
-from .company import Company
-from .dev import Dev
-from .freebie import Freebie
+from company import Company
+from dev import Dev
+from freebie import Freebie
 
 
 __all__ = [ 'Company', 'Dev', 'Freebie' ]
+
+ibm = Company("IBM", 1980)
+facebook = Company("Facebook", 2006)
+
+rooj = Dev("Rooj")
+isaiah = Dev("isaiah")
+
+freebie1 = Freebie(rooj, ibm, "Hat", 5)
+freebie2 = Freebie(isaiah, facebook, "Mug", 3)
+
+
+# print(freebie1.print_details())
+
+# facebook.give_freebie(rooj, "shirt", 10)
+# print(rooj.freebies())
+
+# rooj.give_away(isaiah, freebie1)
+
+print(Company.oldest_company())
--- lib/dev.py
+++ lib/dev.py
@@ -1,4 +1,39 @@
-from .freebie import Freebie
+from freebie import Freebie
 
 class Dev:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str):
+            self._name = value
+
+    def freebies(self):
+        from freebie import Freebie
+        return [freebie for freebie in Freebie.all if freebie.dev == self]
+    
+    def company(self):
+        return (set([freebie.company for  freebie in self.freebies()]))
+    
+    def received_one(self, item_name):
+        received = False
+        for freebie in self.freebies():
+            if freebie.item_name == item_name:
+                return received
+            
+        return received
+    
+    def give_away(self, dev, freebie):
+        if freebie.dev == self:
+            freebie.dev = dev
+
+
+    def __repr__(self):
+        return self.name
+        pass
